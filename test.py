a6f2c67cf57655494b6b23ae5808c855358aff51
matt venn <matt@mattvenn.net>
2020-09-23 11:41:14 +0200

    comments

--- test.py
+++ test.py
@@ -9,15 +9,18 @@ async def test(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.fork(clock.start())
 
+    # open audio files for read and write
     audio_in = wave.open('hello.wav')
     audio_out = wave.open('out.wav', 'wb')
     audio_out.setnchannels(audio_in.getnchannels())
     audio_out.setsampwidth(audio_in.getsampwidth())
     audio_out.setframerate(audio_in.getframerate())
+
     nframes = audio_in.getnframes()
     nframes = 10000
     print("sending %d frames" % nframes)
     
+    # process the audio through the dut
     for i in range(nframes):
         frame = audio_in.readframes(1)
         val, = struct.unpack('h', frame)
@@ -26,4 +29,3 @@ async def test(dut):
 
         raw_out = struct.pack('h', dut.data_out.value.signed_integer)
         audio_out.writeframes(raw_out)
-    
